74c7f698542d1f4f5faf52d2983062c8e9d98004
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2016-09-16 13:55:41 -0300

    configurações do rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,15 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
+RSpec.configure do |config|
+  config.color_enabled? true
+  config.tty = true
+  config.formatter = :documentation
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
+
 require 'business-br'
 require 'business-br/cep'
+require 'business-br/cpf'
