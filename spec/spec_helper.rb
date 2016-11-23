df43a0844e90be18bf455d14b1fb051089166494
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2016-11-23 18:16:12 -0200

    testando em uma versão mais antiga do ruby

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,19 +13,19 @@ RSpec.configure do |config|
 end
 
 def postmon_mock
-  response = '{"complemento": "de 618 a 1188 - lado par", "bairro": "Jardim Paulista", "cidade": "S\u00e3o Paulo", "logradouro": "Alameda Ja\u00fa", "estado_info": {"area_km2": "248.222,362", "codigo_ibge": "35", "nome": "S\u00e3o Paulo"}, "cep": "01420002", "cidade_info": {"area_km2": "1521,11", "codigo_ibge": "3550308"}, "estado": "SP"}'
+  response = '{"complemento": "de 618 a 1188 - lado par", "bairro": "Jardim Paulista", "cidade": "São Paulo", "logradouro": "Alameda Jaú", "estado_info": {"area_km2": "248.222,362", "codigo_ibge": "35", "nome": "São Paulo"}, "cep": "01420002", "cidade_info": {"area_km2": "1521,11", "codigo_ibge": "3550308"}, "estado": "SP"}'
 
   stub_request(:get, "http://api.postmon.com.br/v1/cep/01420002").
-    with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'api.postmon.com.br', 'User-Agent'=>'rest-client/2.0.0 (linux-gnu x86_64) ruby/2.3.1p112'}).
-    to_return(:status => 200, :body => response, :headers => {})
+    with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'api.postmon.com.br', 'User-Agent'=>'rest-client/2.0.0 (linux-gnu x86_64) ruby/2.1.0p0'}).
+    to_return(:status => 200, :body => response, :headers => {'Content-type' => 'application/json'})
 end
 
 def republicavirtual_mock
-  response = '{"resultado":"1","resultado_txt":"sucesso - cep completo","uf":"SP","cidade":"S\u00e3o Paulo","bairro":"Jardim Paulista","tipo_logradouro":"Alameda","logradouro":"Ja\u00fa"}'
+  response = '{"resultado":"1","resultado_txt":"sucesso - cep completo","uf":"SP","cidade":"São Paulo","bairro":"Jardim Paulista","tipo_logradouro":"Alameda","logradouro":"Jaú"}'
 
   stub_request(:get, "http://cep.republicavirtual.com.br/web_cep.php?cep=01420002&formato=json").
-    with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'cep.republicavirtual.com.br', 'User-Agent'=>'rest-client/2.0.0 (linux-gnu x86_64) ruby/2.3.1p112'}).
-    to_return(:status => 200, :body => response, :headers => {})
+    with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip, deflate', 'Host'=>'cep.republicavirtual.com.br', 'User-Agent'=>'rest-client/2.0.0 (linux-gnu x86_64) ruby/2.1.0p0'}).
+    to_return(:status => 200, :body => response, :headers => {'Content-type' => 'application/json'})
 end  
 
 require 'business-br'
