e6d5b888140459079a575519c4fdbb9d15d655be
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2017-01-19 15:17:56 -0200

    updating cep example

--- examples/cep.rb
+++ examples/cep.rb
@@ -1,15 +1,35 @@
 #!/usr/bin/env ruby
 
-$lib = File.expand_path('../lib/')
-$LOAD_PATH.unshift $lib unless $LOAD_PATH.include? $lib
-
-require 'json'
-require 'rest-client'
 require 'business-br'
 
+cep = '13330-000'
+validator = Business::BR::CEP.new
+
+# turns from '13330-000' into '13330000'
+cep = validator.normalize(cep)
+
+
+# turns from '13330000' into '13330-000'
+cep = validator.format(cep)
+
+# getting cep region
+cep_region = validator.region(cep) # => ['SP']
+
+# getting cep type
+cep_type = validator.type(cep) # => 'LOGRADOURO'
+
+
+# check if cep is valid
+if validator.valid? '00000asd'
+  puts 'This CEP is valid'
+else 
+  puts 'This CEP is not valid... please try again!'
+end
+
+# getting cep information
 cep = Business::BR::CEP.new 
-puts cep.search_by('13345460')
-
-
-#response = RestClient.get "http://api.postmon.com.br/v1/cep/13345360"
-#puts JSON.parse(response.body)
+if infor = cep.search_by('12345678')
+  puts "CEP: #{infor}"
+else
+  puts "NOT FOUND"
+end
