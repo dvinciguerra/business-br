762d3652a9e4354959e6b3c35852c5c1da825e00
Daniel Vinciguerra <daniel.vinciguerra@resultadosdigitais.com.br>
2019-01-11 10:23:26 -0200

    Add coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,15 @@ require 'business-br/cep/providers'
 # mock
 require 'webmock/rspec'
 
+# coverage
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start do
+    minimum_coverage 90
+  end
+end
+
+
 RSpec.configure do |config|
   config.color_enabled? true
   config.tty = true
@@ -20,17 +29,16 @@ RSpec.configure do |config|
   end
 end
 
-def postmon_mock
+def postmon_mock(response = {})
   response = '{"complemento": "de 618 a 1188 - lado par", "bairro": "Jardim Paulista", "cidade": "São Paulo", "logradouro": "Alameda Jaú", "estado_info": {"area_km2": "248.222,362", "codigo_ibge": "35", "nome": "São Paulo"}, "cep": "01420002", "cidade_info": {"area_km2": "1521,11", "codigo_ibge": "3550308"}, "estado": "SP"}'
 
   stub_request(:get, "http://api.postmon.com.br/v1/cep/01420002").
     to_return(:status => 200, :body => response, :headers => {'Content-type' => 'application/json'})
 end
 
-def republicavirtual_mock
+def republicavirtual_mock(response = {})
   response = '{"resultado":"1","resultado_txt":"sucesso - cep completo","uf":"SP","cidade":"São Paulo","bairro":"Jardim Paulista","tipo_logradouro":"Alameda","logradouro":"Jaú"}'
 
   stub_request(:get, "http://cep.republicavirtual.com.br/web_cep.php?cep=01420002&formato=json").
     to_return(:status => 200, :body => response, :headers => {'Content-type' => 'application/json'})
-end  
-
+end
