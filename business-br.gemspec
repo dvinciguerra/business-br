19c155b64ac204d26c3ca027f1cca836f8c8f442
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2020-06-08 14:26:15 -0300

    feat: add some configs and fix travis config file

--- business-br.gemspec
+++ business-br.gemspec
@@ -12,20 +12,32 @@ Gem::Specification.new do |spec|
   spec.authors       = ['Daniel Vinciguerra']
   spec.email         = ['daniel.vinciguerra@bivee.com.br']
 
-  spec.summary       = 'This project provide classes for validations and conversions to use in brazilian ruby projects.'
-  spec.description   = 'Business::BR is a namespace to place all validations like CPF, CNPJ, CEP and some other things to be used in a brazilian ruby project.'
+  spec.summary       = %w[
+    Classes for validations and conversions to use in brazilian ruby projects.
+  ]
+
+  spec.description = %w[
+    Business::BR is a namespace to place all validations like CPF CNPJ CEP and
+    some other things to be used in a brazilian ruby project.
+  ]
+
   spec.homepage      = 'https://github.com/dvinciguerra/business-br'
 
-  spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
+  spec.files         = `git ls-files -z`.split("\x0").reject do |filename|
+    filename.match(%r{^(test|spec|features)/})
+  end
+
   spec.bindir        = 'exe'
   spec.require_paths = ['lib']
-  spec.required_ruby_version = '>= 2.0.0'
+
+  spec.required_ruby_version = '>= 2.3.0'
 
   # dependency
-  spec.add_development_dependency 'bundler', '~> 2.1'
-  spec.add_development_dependency 'rake', '~> 12.3'
-  spec.add_development_dependency 'rspec', '~> 3.0'
-  spec.add_development_dependency 'webmock', '>= 2.1.0', '~> 3.8'
+  spec.add_development_dependency 'bundler',    '~> 2.1'
+  spec.add_development_dependency 'rake',       '~> 12.3'
+  spec.add_development_dependency 'rspec',      '~> 3.0'
+  spec.add_development_dependency 'simplecov',  '~> 0.18'
+  spec.add_development_dependency 'webmock',    '>= 2.1.0', '~> 3.8'
 
   spec.add_dependency 'rest-client', '~> 2.0'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 
+require 'json'
 require 'business-br'
 require 'business-br/cep'
 require 'business-br/cpf'
@@ -11,6 +12,9 @@ require 'business-br/cep/providers'
 
 # mock
 require 'webmock/rspec'
+
+require 'simplecov'
+SimpleCov.start if ENV['COVERAGE']
 
 RSpec.configure do |config|
   config.color_enabled? true
@@ -23,15 +27,47 @@ RSpec.configure do |config|
 end
 
 def postmon_mock
-  response = '{"complemento": "de 618 a 1188 - lado par", "bairro": "Jardim Paulista", "cidade": "São Paulo", "logradouro": "Alameda Jaú", "estado_info": {"area_km2": "248.222,362", "codigo_ibge": "35", "nome": "São Paulo"}, "cep": "01420002", "cidade_info": {"area_km2": "1521,11", "codigo_ibge": "3550308"}, "estado": "SP"}'
+  response = {
+    "complemento": 'de 618 a 1188 - lado par',
+    "bairro": 'Jardim Paulista',
+    "cidade": 'São Paulo',
+    "logradouro": 'Alameda Jaú',
+    "estado_info": {
+      "area_km2": '248.222,362',
+      "codigo_ibge": '35',
+      "nome": 'São Paulo'
+    },
+    "cep": '01420002',
+    "cidade_info": {
+      "area_km2": '1521,11',
+      "codigo_ibge": '3550308'
+    },
+    "estado": 'SP'
+  }.to_json
 
   stub_request(:get, 'http://api.postmon.com.br/v1/cep/01420002')
-    .to_return(status: 200, body: response, headers: { 'Content-type' => 'application/json' })
+    .to_return(
+      status: 200,
+      body: response,
+      headers: { 'Content-type' => 'application/json' }
+    )
 end
 
 def republicavirtual_mock
-  response = '{"resultado":"1","resultado_txt":"sucesso - cep completo","uf":"SP","cidade":"São Paulo","bairro":"Jardim Paulista","tipo_logradouro":"Alameda","logradouro":"Jaú"}'
+  response = {
+    "resultado": '1',
+    "resultado_txt": 'sucesso - cep completo',
+    "uf": 'SP',
+    "cidade": 'São Paulo',
+    "bairro": 'Jardim Paulista',
+    "tipo_logradouro": 'Alameda',
+    "logradouro": 'Jaú'
+  }.to_json
 
   stub_request(:get, 'http://cep.republicavirtual.com.br/web_cep.php?cep=01420002&formato=json')
-    .to_return(status: 200, body: response, headers: { 'Content-type' => 'application/json' })
+    .to_return(
+      status: 200,
+      body: response,
+      headers: { 'Content-type' => 'application/json' }
+    )
 end
